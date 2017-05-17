64a695d639a027a596cd12252db1b7b7bd4be3a9
Misha Borsukevich <borsukevich.misha@gmail.com>
2017-05-17 15:49:34 +0300

    v1.1

---
+++ gulp-template/gulpfile.js
@@ -0,0 +1,216 @@
+///////////////////////////////////////////////////
+// 
+// Требуемые плагины
+// 
+///////////////////////////////////////////////////
+
+var gulp = require("gulp"),
+	plugins = require('gulp-load-plugins')(),
+	browserSync = require("browser-sync"),
+	reload = browserSync.reload,
+	rimraf = require("rimraf");
+
+///////////////////////////////////////////////////
+// 
+// Создание переменных конфигурации
+//
+///////////////////////////////////////////////////
+
+var path = {
+	build: {
+		html: 'build/',
+		js: 'build/js/',
+		css: 'build/css/',
+		img: 'build/img/',
+		fonts: 'build/fonts/'
+	},
+	src: {
+		html: 'src/*.html',
+		js: 'src/js/scripts.js',
+		style: 'src/style/styles.scss',
+		css: 'src/style/',
+		img: 'src/img/**/*.*',
+		fonts: 'src/fonts/**/*.*',
+		templates: 'src/templates/**/*.html'
+	},
+	watch: {
+		html: 'src/**/*.html',
+		js: 'src/js/**/*.js',
+		style: 'src/style/**/*.scss',
+		img: 'src/img/**/*.*',
+		fonts: 'src/fonts/**/*.*'
+	},
+	clean: './build',
+	bootstrap: {
+		src: ['node_modules/bootstrap/dist/css/bootstrap.css',
+		'node_modules/bootstrap/dist/css/bootstrap.min.css'],
+		dist: 'src/style/',
+		buildWhat: ['src/style/bootstrap.css',
+		'src/style/bootstrap.min.css'],
+		buildWhere: 'build/css/bootstrap/'
+	},
+	jquery: {
+		src: 'node_modules/jquery/dist/jquery.min.js',
+		dist: 'src/js/jquery/',
+		buildWhat: 'src/js/jquery/*.js',
+		buildWhere: 'build/js/jquery/'
+	}
+};
+
+///////////////////////////////////////////////////
+//
+// Задачи для HTML
+//
+///////////////////////////////////////////////////
+
+gulp.task("html:build", function() {
+	gulp.src(path.src.html)
+	.pipe(plugins.rigger())
+	.pipe(gulp.dest(path.build.html))
+	.pipe(reload({stream: true}));
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи для стилей
+//
+//////////////////////////////////////////////////
+
+gulp.task("style:build", function() {
+	gulp.src(path.bootstrap.src)
+	.pipe(gulp.dest(path.bootstrap.dist));
+	gulp.src(path.bootstrap.buildWhat)
+	.pipe(gulp.dest(path.bootstrap.buildWhere));
+	gulp.src(path.src.style)
+	.pipe(plugins.sass())
+	.pipe(gulp.dest(path.src.css));
+	gulp.src(path.bootstrap.buildWhat)
+	.pipe(plugins.concatCss('styles.css'))
+	.pipe(plugins.uncss({
+		html: [path.src.templates]
+	}))
+	.pipe(plugins.rename({suffix: '.min'}))
+	.pipe(gulp.dest(path.build.css))
+	.pipe(reload({stream: true}));
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи для скриптов
+//
+//////////////////////////////////////////////////
+
+gulp.task("js:build", function() {
+	gulp.src(path.jquery.src)
+	.pipe(gulp.dest(path.jquery.dist));
+	gulp.src(path.jquery.buildWhat)
+	.pipe(gulp.dest(path.jquery.buildWhere));
+	gulp.src(path.src.js)
+	.pipe(plugins.rigger())
+	.pipe(plugins.uglify())
+	.pipe(plugins.rename({suffix: '.min'}))
+	.pipe(gulp.dest(path.build.js))
+	.pipe(reload({stream: true}));
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи для изображений
+//
+///////////////////////////////////////////////////
+
+gulp.task("image:build", function() {
+	gulp.src(path.src.img)
+	.pipe(plugins.imagemin({
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [plugins.pngmin()],
+		interlaced: true
+	}))
+	.pipe(gulp.dest(path.build.img))
+	.pipe(reload({stream: true}));
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи для шрифтов
+//
+///////////////////////////////////////////////////
+
+gulp.task("fonts:build", function() {
+	gulp.src(path.src.fonts)
+	.pipe(gulp.dest(path.build.fonts));
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи Browser-Sync
+//
+///////////////////////////////////////////////////
+
+gulp.task("browserSync", function() {
+	browserSync({
+		server: {
+			baseDir: "./build"
+		},
+		port: 8080,
+		open: true,
+		notify: false
+	});
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи для очистки
+//
+///////////////////////////////////////////////////
+
+gulp.task("clean", function(cb) {
+	rimraf(path.clean, cb);
+});
+
+///////////////////////////////////////////////////
+//
+// Задачи сборки
+//
+///////////////////////////////////////////////////
+
+gulp.task("build", ["clean"], function() {
+	gulp.start("html:build");
+	gulp.start("style:build");
+	gulp.start("js:build");
+//	gulp.start("image:build");
+	gulp.start("fonts:build");
+});
+
+///////////////////////////////////////////////////
+//
+// Задача watch
+//
+///////////////////////////////////////////////////
+
+gulp.task("watch", function() {
+	gulp.watch([path.watch.html], function() {
+		gulp.start("html:build");
+	});
+	gulp.watch([path.watch.style], function() {
+		gulp.start("style:build");
+	});
+	gulp.watch([path.watch.js], function() {
+		gulp.start("js:build");
+	});
+	gulp.watch([path.watch.img], function() {
+		gulp.start("image:build");
+	});
+	gulp.watch([path.watch.fonts], function() {
+		gulp.start("fonts:build");
+	});
+});
+
+///////////////////////////////////////////////////
+//
+// Задача по умолчанию
+//
+///////////////////////////////////////////////////
+
+gulp.task("default", ["build", "browserSync", "watch"]);
